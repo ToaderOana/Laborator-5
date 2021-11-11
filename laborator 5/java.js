efc08b7f8a5779cc0feb8a89f0b6d9a81fe452ff
Toader Oana <77963503+ToaderOana@users.noreply.github.com>
2021-11-11 19:28:39 +0200

    Add files via upload

---
+++ laborator 5/java.js
@@ -0,0 +1,8 @@
+function afisare()
+{
+	<button type="button" onclick='afisare("nume").innerHTML="Ing. Toader Oana"'>Alta viata</button>
+	<button type="button" onclick='afisare("1").innerHTML="Volei de performanta"'>Alta viata</button>
+	<button type="button" onclick='afisare("2").innerHTML="Majorete"'>Alta viata</button>
+	<button type="button" onclick='afisare("3").innerHTML="Cursuri make-up"'>Alta viata</button>
+	<button type="button" onclick='afisare("4").innerHTML="Informatica aprofundata"'>Alta viata</button>
+}
